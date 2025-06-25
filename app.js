bb4bc1d146f97d8af782bdf3ace9311a3ddc752a
TrivmviraT <consvpr@outlook.es>
2025-06-25 19:59:34 +0200

    denuevo

--- app.js
+++ app.js
@@ -1,30 +1,36 @@
-const juego =["piedra", "papel", "tijeras","lagarto", "spock"];
+const game =["rock", "paper", "scissors","lizard", "spock"];
 
-const eleccionPC= Math.floor(Math.random()*juego.length);
+const pcChoice= Math.floor(Math.random()*game.length);
 
 
-console.log("El PC elige:"+juego[eleccionPC]);
+console.log("El PC elige:"+game[pcChoice]);
 
-const empezarJuego = (eleccionJugador) =>{
-// Robert me ha ayudado a hacer esta parte con el !juego.includes   
-if (!juego.includes(eleccionJugador)) {
-    return console.log("Qué estás haciendo ?")    
+const startGame = (playerChoice) =>{
+  
+    if (!juego.includes(playerChoice)) 
+    {
+        return console.log("What are doin´ ?")    
+    }
+    else if (
+        (playerChoice === "rock" && (game[pcChoice]==="scissors" || game[pcChoice]==="lizard")) ||
+        (playerChoice === "paper" && (game[pcChoice]==="rock" || game[pcChoice]==="spock")) ||
+        (playerChoice === "scissors" && (game[pcChoice]==="paper" || game[pcChoice]==="lizard")) ||
+        (playerChoice === "lizard" && (game[pcChoice]==="spock" || game[pcChoice]==="paper")) ||
+        (playerChoice === "spock" && (game[pcChoice]==="scissors" || game[pcChoice]==="rock"))  
+    )
+    {
+        console.log("You Win!!!");
+    }
+
+    else if(playerChoice===game[pcChoice])
+    {
+        console.log("Draw !!!");
+    }
+    else 
+    {
+        console.log("You Lose!!!");
+    }
+    console.log("You choose:", playerChoice);
 }
-else if (
-    (eleccionJugador === "piedra" && (juego[eleccionPC]==="tijeras" || juego[eleccionPC]==="lagarto")) ||
-    (eleccionJugador === "papel" && (juego[eleccionPC]==="piedra" || juego[eleccionPC]==="spock")) ||
-    (eleccionJugador === "tijeras" && (juego[eleccionPC]==="papel" || juego[eleccionPC]==="lagarto")) ||
-    (eleccionJugador === "lagarto" && (juego[eleccionPC]==="spock" || juego[eleccionPC]==="papel")) ||
-    (eleccionJugador === "spock" && (juego[eleccionPC]==="tijeras" || juego[eleccionPC]==="piedra"))  
-)
-{console.log("You Win!!!");}
 
-else if(eleccionJugador===juego[eleccionPC])
-{console.log("Empate !!!");}
-else {
-    console.log("You Lose!!!");
-}
-console.log("Tú eliges:", eleccionJugador);
-}
-
-empezarJuego("papel");
+startGame("paper");
